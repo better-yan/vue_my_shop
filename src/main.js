36b19dde9189387b8831ef0343ab73bf2833f496
better-yan <1696762834@qq.com>
2021-03-25 16:04:53 +0800

    完成商品分类

--- src/main.js
+++ src/main.js
@@ -4,17 +4,18 @@ import router from './router'
 import './plugins/element.js'
 // 导入全局样式表
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
 //请求路径配置
 axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1/'
-axios.interceptors.request.use(config=>{
+axios.interceptors.request.use(config => {
   console.log(config);
-  config.headers.Authorization=window.sessionStorage.getItem('token');
+  config.headers.Authorization = window.sessionStorage.getItem('token');
   return config;
 })
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
+Vue.component('tree-table', TreeTable)
 new Vue({
   router,
   render: h => h(App)
